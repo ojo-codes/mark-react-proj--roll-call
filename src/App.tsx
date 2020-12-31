a2ad3bb44e1e603acbd76431e7dd7e942ebd9ac5
Richard Ng <richardcrng@gmail.com>
2020-12-31 10:55:24 +0000

    Set up basic project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Roll call!</h1>
+      <Character
+        name="Medusa"
+        nickname="the one with snakes in her hair"
+        location="Sarpedon"
+        hobbies={["turning people to stone", "baking sourdough bread"]}
+        origin="Greek Mythology"
+      />
+      <Character
+        name="Hercules"
+        nickname="Mr Muscle"
+        location="Rome"
+        hobbies={[
+          "slaying evil",
+          "flexing in togas",
+          "unnecessary side-quests",
+        ]}
+        origin="Roman Mythology"
+      />
+      <Character
+        name="Storm"
+        nickname="Supercharged Halle Berry"
+        location="New York"
+        hobbies={["floating menacingly in the air", "looking fleek"]}
+        origin="Roman Mythology"
+      />
+    </div>
+  );
+}
+
+interface CharacterProps {
+  name: string;
+  nickname: string;
+  location: string;
+  hobbies: string[];
+  origin: "Greek Mythology" | "Roman Mythology" | "Marvel Comics" | "DC Comics";
+}
+
+function Character(props: CharacterProps) {
+  return (
+    <div className="character-card">
+      <h2>{props.name.toUpperCase()}</h2>
+      <h3>the legendary</h3>
+      <p>
+        <i>Good to have you here, {props.name}!</i>
+      </p>
+      <p>
+        <b>Location:</b> {props.location}
+      </p>
+      <p>
+        <b>Hobbies:</b> {props.hobbies.join(", ")}
+      </p>
+      <p>{props.name} originates from props.origin</p>
     </div>
   );
 }
